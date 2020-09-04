3b934a7931a8128a0161591894e54ed1aac7de13
CucciPro <josej@email.arizona.edu>
2020-09-04 10:39:55 -0700

    Setting up initial questions

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a description of the project.'
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'Provide a step-by-step description of how to get the development environment running.'
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use.'
+      },
+      {
+        type: 'input',
+        name: 'Contribution',
+        message: 'List your collaborators, if any.'
+      },
+      {
+        type: 'input',
+        name: 'Test',
+        message: 'Write tests for your application.'
+      },
+      
 ];
 
 // function to write README file
