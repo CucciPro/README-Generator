5afa10eed14b04b7110f64085a74a016d7c08e56
CucciPro <josej@email.arizona.edu>
2020-09-04 15:03:35 -0700

    set up README template that will use provided data

--- index.js
+++ index.js
@@ -10,34 +10,44 @@ const questions = [
       {
         type: 'input',
         name: 'Description',
-        message: 'Provide a description of the project.'
+        message: 'Provide a description of the project:'
       },
       {
         type: 'input',
         name: 'Installation',
-        message: 'Provide a step-by-step description of how to get the development environment running.'
+        message: 'Provide a step-by-step description of how to get the development environment running:'
       },
       {
         type: 'input',
         name: 'Usage',
-        message: 'Provide instructions and examples for use.'
+        message: 'Provide instructions and examples for use:'
       },
       {
         type: 'input',
         name: 'Contribution',
-        message: 'List your collaborators, if any.'
+        message: 'Provide any contribution guidelines:'
       },
       {
         type: 'input',
         name: 'Test',
-        message: 'Write tests for your application.'
+        message: 'Provide test examples for your application:'
       },
       {
         type: 'checkbox',
         name: 'License',
-        message: 'What license are you using in this project? (Check one)',
+        message: 'What license are you using in this project? (Select one)',
         choices: ['mit', 'apache-2.0', 'afl-3.0', 'artistic-2.0', 'wtfpl', 'gpl', 'mpl']
-      }, 
+      },
+      {
+        type: 'input',
+        name: 'Github',
+        message: 'Provide your professional Github username:'
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter email where users can submit questions or concerns:'
+      },
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,52 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
+
+## Description 
+
+${data.Description}
+
+## Badges
+
+![](https://img.shields.io/static/v1?label=license&message=${data.License}&color=brightgreen?style=plastic&logo=appveyor)
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributing)
+* [License](#license)
+
+
+## Installation
+
+${data.Installation}
+
+
+## Usage 
+
+${data.Usage} 
+
+
+## License
+
+This project is licensed under the ${data.License} license.
+
+
+## Contributing
+
+${data.Contributions} 
+
+
+## Tests
+
+Run the following commmands to run a test:
+
+${data.Test} 
+
+## Questions
+
+For any questions please reach out on [Github]( https://github.com/${data.Github}/) or directly to my [email](${data.Email})!
 
 `;
 }
